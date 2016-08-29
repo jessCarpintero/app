bf40eec8e4de5cbb8daf56aae8673e78217fe746
jessCarpintero <j.c.m25@outlook.com>
2016-08-29 12:36:12 +0200

    Pequeños cambios

--- src/main/webapp/js/controller.js
+++ src/main/webapp/js/controller.js
@@ -16,38 +16,38 @@ angular.module('appEdicom', ['ngRoute'])
 		var password = $scope.domain;
 		var domain = $scope.password;
 		$log.debug("Username: " + username + "\n Pass: " + password  + "\n Domain: " + domain );
-//		$http.get("http://localhost/rest/userservice/useraccess?username=" + username + "&password=" + password + "&domain=" + domain )
-//        .then(function(res){
-//        	$http.get('getFolders?type=J').then(function(response) {
-//        	    $http({
-//        	      url : 'https://webpre.sedeb2b.com/EdiwinES/services/mobile',
-//        	      method : 'GET',
-//        	      headers : {
-//        	        'Authorization' :  angular.fromJson(res).token
-//        	      }
-//        	    }).then(function(response) {
-//        	      angular.fromJson(response).description;
-//        	      $log.debug(angular.fromJson(res).token);	
-//        	    });   
-//        	});
-//        	$location.path('/folders');        	
-//        }, function(res){
-//          $scope.error = [{name: "Error!! " + res.status}];
-//        });
+		$http.get("http://localhost/rest/userservice/useraccess?username=" + username + "&password=" + password + "&domain=" + domain )
+        .then(function(res){
+        	$http.get('https://webpre.sedeb2b.com/EdiwinES/services/mobile/getFolders?type=J').then(function(response) {
+        	    $http({
+        	      method : 'GET',
+        	      dataType: 'json',
+        	      headers : {
+        	        'Authorization' : 'Basic ' + angular.fromJson(res).token
+        	      }
+        	    }).then(function(response) {
+        	      var folders = angular.fromJson(response);
+        	      $log.debug("Description folder: " + folders.description);	
+        	    });   
+        	});
+        	$location.path('/folders');        	
+        }, function(res){
+          $scope.error = [{name: "Error!! " + res.status}];
+        });
 		
-   	      $http({
-    	      url : 'https://webpre.sedeb2b.com/EdiwinES/services/mobile/registerSession?group=ASPEDITEST' + '&domain=' + domain + '&user=' + username + '&password=' + password + '&ediwinini=EDIWIN_ASPTEST.INI',
-    	      method : 'GET',
-    	      dataType: 'json',
-    	      headers : {
-    	    	  'Access-Control-Allow-Methods': 'GET,POST,OPTIONS',
-    	    	  'Access-Control-Allow-Headers' : 'Content-Type',
-    	    	  'Access-Control-Allow-Origin' :  '*'
-    	      }
-    	    }).success(function(data){
-    	    	var securityToken = angular.fromJson(data);
-            	$log.debug("Value token: " + securityToken.token);
-            	$location.path('/folders');        	
-            });  
+//   	      $http({
+//    	      url : 'https://webpre.sedeb2b.com/EdiwinES/services/mobile/registerSession?group=ASPEDITEST' + '&domain=' + domain + '&user=' + username + '&password=' + password + '&ediwinini=EDIWIN_ASPTEST.INI',
+//    	      method : 'GET',
+//    	      dataType: 'json',
+//    	      headers : {
+//    	    	  'Access-Control-Allow-Methods': 'GET,POST,OPTIONS',
+//    	    	  'Access-Control-Allow-Headers' : 'Content-Type',
+//    	    	  'Access-Control-Allow-Origin' :  '*'
+//    	      }
+//    	    }).success(function(data){
+//    	    	var securityToken = angular.fromJson(data);
+//            	$log.debug("Value token: " + securityToken.token);
+//            	$location.path('/folders');        	
+//            });  
 	};
 });
